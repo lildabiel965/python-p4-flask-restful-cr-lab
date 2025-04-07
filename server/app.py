aa3c1738259a3bab709643b991c776b9d3569125
Daniel <daniel.mutembei@student.moringaschool.com>
2025-04-07 09:11:57 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,11 +17,34 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        plants = Plant.query.all()
+        plants_dict = [plant.to_dict() for plant in plants]
+        return make_response(plants_dict, 200)
+        
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+        return make_response(plant.to_dict(), 200)
         
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Numeric
 
 db = SQLAlchemy()
 
@@ -7,3 +8,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(Numeric(precision=10, scale=2))
